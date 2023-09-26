4895ca570d7ea15d3aefa9cbc2beb69be95b506e
Brent Dolan <94938468+brentdolan@users.noreply.github.com>
2023-09-26 11:55:02 -0500

    nothing important

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <HeroSection/>
+        <img alt={'Figma test image'} src={'/figma.png'}/>
     </main>
   )
 }
