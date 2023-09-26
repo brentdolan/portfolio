57b0edb6869193abe4d451a5452d6a4d2d5c8df2
Brent Dolan <94938468+brentdolan@users.noreply.github.com>
2023-09-26 11:58:25 -0500

    nothing important

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <HeroSection/>
-        <img alt={'Figma test image'} src={'/figma.png'}/>
+        <img alt={'Figma test image'} src={'/realFigma.png'}/>
     </main>
   )
 }
